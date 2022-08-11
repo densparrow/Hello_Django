8da2316be5ee25983724e4e0a8a41da2765fa3cd
densparrow <dzianisbohush@gmail.com>
2022-08-11 11:58:37 +0300

    start_files

--- den_2022/den_2022/urls.py
+++ den_2022/den_2022/urls.py
@@ -1,4 +1,4 @@
-"""den_2021 URL Configuration
+"""den_2022 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
