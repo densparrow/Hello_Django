3bf51676703990d5fe6144c3666a98b14766efd9
densparrow <dzianisbohush@gmail.com>
2022-08-12 20:43:50 +0300

    Удалил hello_world приложение. Создал новое приложение с названием project. Создал папку templates. Создайл файл url.py Включил URL данного приложения к основному каталогу

--- Den_2022/project/urls.py
+++ Den_2022/project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hello_world/",views.hello_world)
+    path("project/",views.project)
 ]
---
+++ Den_2022/project/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+from django.shortcuts import render
+
+def project(request):
+    return render(request,'project.html')
